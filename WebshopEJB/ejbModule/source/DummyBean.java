159e92ab3bc434d338e6ba96518fc89151b8dd21
Keller Dominic thkeller <dominic.keller@stud.hslu.ch>
2015-10-15 21:40:10 +0200

    inital commit

---
+++ WebshopEJB/ejbModule/source/DummyBean.java
@@ -0,0 +1,20 @@
+package source;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class DummyBean
+ */
+@Stateless
+@LocalBean
+public class DummyBean {
+
+    /**
+     * Default constructor. 
+     */
+    public DummyBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
